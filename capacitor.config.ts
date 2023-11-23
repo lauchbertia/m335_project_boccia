5783b49ccf66b978f9744b02977fc3eea2407e39
lauchbertia <sabrina.boccia@bbzbl-it.ch>
2023-11-23 15:19:55 +0100

    wip: splashscreen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -11,8 +11,14 @@ const config: CapacitorConfig = {
     SplashScreen: {
       launchShowDuration: 3000,
       imagePath: 'android\app\resources\splash.png'
-    }
-  }
+    },
+    LocalNotifications: {
+      smallIcon: "ic_stat_icon_config_sample",
+      iconColor: "#488AFF",
+      sound: "beep.wav",
+    },
+  },
+  
 };
 
 export default config;
